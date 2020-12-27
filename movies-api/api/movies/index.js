73aa8fa5a7899716ce9102a338fdc28bc3358cc9
Michael Barcoe <barcoe98@gmail.com>
2020-12-27 14:08:04 +0000

    Week 9 refactor movie api

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,76 +1,22 @@
 import express from 'express';
-import {moviesObject} from './movies';
+import {
+  getMovies, getMovie, getMovieReviews
+} from '../tmdb-api';
 
-const router = express.Router(); // eslint-disable-line
+const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).send(moviesObject);
+router.get('/', (req, res,next) => {
+  getMovies().then(movies => res.status(200).send(movies));
 });
 
+router.get('/:id', (req, res, next) => {
+  const id = parseInt(req.params.id);
+  getMovie(id).then(movie => res.status(200).send(movie));
+});
 
-//add a movie
-router.post('/', (req, res) => {
-    let newMovie = req.body;
-    
-    if (newMovie && newMovie.title) {
-      //Adds a random id if missing. 
-      !newMovie.id ? newMovie.id = Math.round(Math.random() * 10000) : newMovie 
-      movieObject.movies.push(newMovie);
-      res.status(201).send(newMovie);
-    } else {
-      res.status(405).send({
-        message: "Invalid Movie Data",
-        status: 405
-      });
-    }
-  });
-
-//get a movie
-router.get('/:id', (req, res) => {
-    const key =  parseInt(req.params.id);
-    const index = moviesObject.movies.map((movie)=>{
-  return movie.id;
-  }).indexOf(key);
-   if (index > -1) {
-       res.status(200).send(moviesObject.movies[index]);
-   } else {
-     res.status(404).send({message: `Unable to find movie with id: ${key}.`, status: 404});
-     }
-  });
-
-  
-
-  // Update a movie
-router.put('/:id', (req, res) => {
-    const key = parseInt(req.params.id);
-    const updateMovie = req.body;
-    const index = moviesObject.movies.map((movie) => {
-      return movie.id;
-    }).indexOf(key);
-    if (index !== -1) {
-      !updateMovie.id ? updateMovie.id = key : updateMovie
-      moviesObject.movies.splice(index, 1, updateMovie);
-      res.status(200).send(updateMovie);
-    } else {
-      res.status(404).send({
-        message: 'Unable to find Movie',
-        status: 404
-      });
-    }
-  });
-
-  // Delete a movie
-router.delete('/:id', (req, res) => {
-    const key =  parseInt(req.params.id);
-    const index = moviesObject.movies.map((movie)=>{
-  return movie.id;
-  }).indexOf(key);
-   if (index > -1) {
-    moviesObject.movies.splice(index, 1);
-       res.status(200).send({message: `Deleted movie id: ${key}.`,status: 200});
-   } else {
-     res.status(404).send({message: `Unable to find movie with id: ${key}.`, status: 404});
-     }
-  });
+router.get('/:id/reviews', (req, res, next) => {
+  const id = parseInt(req.params.id);
+  getMovieReviews(id).then(reviews => res.status(200).send(reviews));
+});
 
 export default router;
---
+++ movies-api/api/tmdb-api.js
@@ -0,0 +1,30 @@
+import fetch from 'node-fetch';
+
+export const getMovies = () => {
+    return fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&page=1`
+    )
+      .then(res => res.json())
+      .then(json => json.results);
+  };
+
+  export const getMovie = id => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.TMDB_KEY}`
+    ).then(res => res.json());
+  };
+
+  export const getGenres = () => {
+    return fetch(
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.TMDB_KEY}&language=en-US`
+    ).then(res => res.json())
+    .then(json => json.genres);
+  };
+
+  export const getMovieReviews = id => {
+    return fetch(
+      `https://api.themovied.org/3/movie/${id}/reviews?api_key=${process.env.TMDB_KEY}`
+    )
+      .then(res => res.json())
+      .then(json => json.results);
+  };
