1e309ab915cf39b3efef5e8724c352098b03bd78
Michael Barcoe <barcoe98@gmail.com>
2020-12-29 17:09:05 +0000

    Week 9 User Favourites

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -5,7 +5,7 @@ const router = express.Router(); // eslint-disable-line
 
 // Get all users
 router.get('/', (req, res, next) => {
-    User.find().then(users =>  res.status(200, users).json({success:true,token:"FakeTokenForNow"})).catch(next);
+    User.find().then(users =>  res.status(200).json(users)).catch(next);;
 });
 
 // register
@@ -21,6 +21,32 @@ router.put('/:id',  (req, res, next) => {
     }, req.body, {
       upsert: false,
     })
-    .then(user => res.status(200, user).json({success:true,token:"FakeTokenForNow"})).catch(next);
+    .then(user => res.json(200, user)).catch(next);;
 });
+
+router.post('/:userName/favourites', (req, res, next) => {
+    const newFavourite = req.body;
+    const query = {username: req.params.userName};
+    if (newFavourite && newFavourite.id) {
+      User.find(query).then(
+        user => {
+          (user.favourites)?user.favourites.push(newFavourite):user.favourites =[newFavourite];
+          User.findOneAndUpdate(query, {favourites:user.favourites}, {
+            new: true
+          }).then(user => res.status(201).send(user));
+        }
+      ).catch(next);
+    } else {
+        res.status(401).send("Unable to find user")
+    }
+  });
+
+  router.get('/:userName/favourites', (req, res, next) => {
+    const user = req.params.userName;
+    User.find( {username: user}).then(
+        user => res.status(201).send(user.favourites)
+    ).catch(next);
+  });
+
+  
 export default router;
