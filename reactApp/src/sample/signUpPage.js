ef9f9291f7c4c9b853f223c6ef592cf3c82ca1d3
Michael Barcoe <barcoe98@gmail.com>
2021-01-02 18:59:27 +0000

    Topic 11 - Sign up

--- reactApp/src/sample/signUpPage.js
+++ reactApp/src/sample/signUpPage.js
@@ -1,13 +1,20 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Redirect } from "react-router-dom";
-
+import { AuthContext } from './authContext';
 
 const SignUpPage = props => {
+  const context = useContext(AuthContext)
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordAgain, setPasswordAgain] = useState("");
   const [registered, setRegistered] = useState(false);
 
   const register = () => {
-      setRegistered(true) 
+    if (password.length > 0 && password === passwordAgain) {
+      context.register(userName, password);
+      setRegistered(true);
     }
+  }
 
   const { from } = props.location.state || { from: { pathname: "/" } };
 
@@ -19,6 +26,16 @@ const SignUpPage = props => {
     <>
       <h2>SignUp page</h2>
       <p>You must register a username and password to log in </p>
+      <input value={userName} placeholder="user name" onChange={e => {
+        setUserName(e.target.value);
+      }}></input><br />
+      <input value={password} type="password" placeholder="password" onChange={e => {
+        setPassword(e.target.value);
+      }}></input><br />
+      <input value={passwordAgain} type="password" placeholder="password again" onChange={e => {
+        setPasswordAgain(e.target.value);
+      }}></input><br />
+      {/* Login web form  */}
       <button onClick={register}>Register</button>
     </>
   );
