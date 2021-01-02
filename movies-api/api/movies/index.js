5324d3481aa2d871140c5de265164184753efeef
Michael Barcoe <barcoe98@gmail.com>
2021-01-02 19:43:00 +0000

    Topic 11 - minor changes

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,29 +1,22 @@
 import express from 'express';
-import movieModel from '../../seedData/movies'
-
+import {getMovieReviews} from '../tmdb-api';
 const router = express.Router();
 
 router.get('/', ( res, next) => {
   movieModel.find().then(movies => res.status(200).send(movies)).catch(next);
 });
 
-
-// router.get('/', (req, res,next) => {
-//   getMovies().then(movies => res.status(200).send(movies))
-//   .catch((error) => next(error));;
-// });
-
 router.get('/:id', (req, res, next) => {
   const id = parseInt(req.params.id);
   movieModel.findByMovieDBId(id).then(movie => res.status(200).send(movie).catch(next));
 });
 
-// router.get('/:id/reviews', (req, res, next) => {
-//   const id = parseInt(req.params.id);
-//   getMovieReviews(id)
-//   .then(reviews => res.status(200).send(reviews))
-//   .catch((error) => next(error));
-// });
+router.get('/:id/reviews', (req, res, next) => {
+  const id = parseInt(req.params.id);
+  getMovieReviews(id)
+  .then(reviews => res.status(200).send(reviews))
+  .catch((error) => next(error));
+});
 
 export default router;
 
