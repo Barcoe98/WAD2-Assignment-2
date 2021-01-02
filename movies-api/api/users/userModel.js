8a1739a392fc3c1629b61f1e072849aba6d3bab6
Michael Barcoe <barcoe98@gmail.com>
2021-01-02 13:00:09 +0000

    Topic 10 - Session based Auth

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt-nodejs';
+
 
 const Schema = mongoose.Schema;
 
@@ -17,12 +19,34 @@ UserSchema.statics.findByUserName = function (username) {
   return this.findOne({ username: username });
 };
 
-UserSchema.methods.comparePassword = function (candidatePassword) {
-  const isMatch = this.password === candidatePassword;
-  if (!isMatch) {
-    throw new Error('Password mismatch');
-  }
-  return this;
+UserSchema.methods.comparePassword = function(passw, cb) {
+  bcrypt.compare(passw, this.password, (err, isMatch) => {
+      if (err) {
+          return cb(err);
+      }
+      cb(null, isMatch);
+  });
 };
+
+//encrypts the password property before it is saved or updated
+UserSchema.pre('save', function(next) {
+  const user = this;
+  if (this.isModified('password') || this.isNew) {
+      bcrypt.genSalt(10, (err, salt)=> {
+          if (err) {
+              return next(err);
+          }
+          bcrypt.hash(user.password, salt, null, (err, hash)=> {
+              if (err) {
+                  return next(err);
+              }
+              user.password = hash;
+              next();
+          });
+      });
+  } else {
+      return next();
+  }
+});
 
 export default mongoose.model('User', UserSchema);
--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -16,7 +16,7 @@ export async function loadUsers() {
   console.log('load user Data');
     try {
       await userModel.deleteMany();
-      await userModel.collection.insertMany(users);
+      await users.forEach(user => userModel.create(user));
       console.info(`${users.length} users were successfully stored.`);
     } catch (err) {
       console.error(`failed to Load user Data: ${err}`);
