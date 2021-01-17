9d1eefc0a66179db6cbe5b9eea525fb747b4478a
Michael Barcoe <barcoe98@gmail.com>
2021-01-17 01:40:44 +0000

    added Tv Show Card

---
+++ reactApp/src/components/tvShowCard/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./tvShowCard.css";
+
+const TvShowCard = ({tvShow}) => {
+
+  return (
+    <div className="col-sm-2">
+      <div className="card">
+      <div className="container">
+        <Link to={`/tvshow/${tvShow.id}`}>
+      <img
+      className="card-img-tag center"
+      alt={tvShow.title}
+      src={
+        tvShow.poster_path
+      ? `https://image.tmdb.org/t/p/w500/${tvShow.poster_path}`
+      : "./film-poster-placeholder.png"
+      }
+      />
+      </Link>
+      <h4 className="card-title">{tvShow.title}</h4>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default TvShowCard;
